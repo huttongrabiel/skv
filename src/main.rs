fecf1af9b046cc1ff30fbe2c0638f7174c81f024
huttongrabiel <huttonthomas@icloud.com>
2022-08-23 11:56:15 -0700

    Main+Connection: Use RwLock instead of Mutex for concurrent reads...
    and blocking writes. PUT and DELETE requests are blocking, but GET
    requests are not. With the Mutex shared state concurrency, even GET
    requests were blocking which is not good and essentially ruins the whole
    point of concurrency. Now we can do many reads at once and only lock up
    the whole KeyValueStore when doing a modifying operation.
    
    To do this, we had to be specific about what kind of lock, read() or
    write(), to acquire in main so we have to move some functionality to
    main. And to do that, we have to break apart handle_request into its
    pieces and then call each piece individually.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::Parser;
-use skv::connection::KeyValueStore;
+use skv::connection::{self, KeyValueStore, RequestType};
 use skv::thread::ThreadPool;
 use std::{
     error::Error,
     net::TcpListener,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -18,19 +18,38 @@ fn main() -> Result<(), Box<dyn Error>> {
             )
         });
 
-    let key_value_store = Arc::new(Mutex::new(KeyValueStore::new()));
+    let key_value_store = Arc::new(RwLock::new(KeyValueStore::new()));
 
     let thread_pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
+        let buf = connection::buf_from_stream(&stream)?;
+        connection::verify_request(&buf)?;
+        let request_type = connection::request_type(&buf);
+
         let kv_store = Arc::clone(&key_value_store);
         thread_pool.execute(move || {
-            match kv_store.lock().unwrap().handle_request(stream) {
+            let (status_line, body) = match request_type {
+                RequestType::Get => kv_store
+                    .read()
+                    .expect("Failed to acquire read lock for get request.")
+                    .handle_get_request(&buf),
+                RequestType::Put => kv_store
+                    .write()
+                    .expect("Failed to acquire write lock for PUT request.")
+                    .handle_put_request(&buf),
+                RequestType::Delete => kv_store
+                    .write()
+                    .expect("Failed to acquire write lock for DELETE request.")
+                    .handle_put_request(&buf),
+                RequestType::Unknown(mesg) => mesg,
+            };
+            match connection::write_stream(&stream, status_line, body) {
                 Ok(_) => (),
-                Err(e) => eprintln!("Error, try again: {}", e),
-            }
+                Err(e) => eprintln!("Error encountered: {}", e),
+            };
         });
     }
 
